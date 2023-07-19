7aba8e5a994db3669620394460481e815f6b2921
muratavci05 <js.muratavci@gmail.com>
2023-07-19 16:01:53 +0300

    second commit

---
+++ src/pages/Feed.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Feed = () => {
+  return (
+    <div>
+      <h1 classname="text-3xl font-bold underline text-red-800">Hello world!</h1>
+    </div>
+  );
+};
+
+export default Feed;
